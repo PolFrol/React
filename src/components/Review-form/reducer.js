d3f165fb635fdfe1c92d31986d41d3a91d542e19
Polina Frolova <polfroll@gmail.com>
2025-03-19 23:00:13 +0500

    hw-4

--- src/components/Review-form/reducer.js
+++ src/components/Review-form/reducer.js
@@ -1,4 +1,12 @@
-import { SET_NAME_ACTION, SET_TEXT_ACTION, SET_RATING, DEFAULT_VALUE_FORM, RESET_FORM} from "../../materials/constants"
+export const DEFAULT_VALUE_FORM = {
+    name: "",
+    text: "",
+    rating: 1
+}
+export const SET_NAME_ACTION = "SET_NAME_ACTION";
+export const SET_TEXT_ACTION = "SET_TEXT_ACTION";
+export const SET_RATING = "SET_RATING";
+export const RESET_FORM = "RESET_FORM";
 
 export const reducer = (state, { type, payload }) => {
     switch (type) {
--- src/components/Review-form/use-form-reducer.js
+++ src/components/Review-form/use-form-reducer.js
@@ -1,10 +1,10 @@
-import { useReducer } from "react"
-import { DEFAULT_VALUE_FORM, RESET_FORM, SET_NAME_ACTION, SET_RATING, SET_TEXT_ACTION } from "../../materials/constants"
-import { reducer } from "./reducer"
+import { useReducer } from "react";
+import { DEFAULT_VALUE_FORM, RESET_FORM, SET_NAME_ACTION, SET_RATING, SET_TEXT_ACTION, reducer } from "./reducer"
+
+const MIN = 1;
+const MAX = 5;
 
 export const useFormReducer = () => {
-    const MIN = 1;
-    const MAX = 5;
     const [form, dispatch] = useReducer(reducer, DEFAULT_VALUE_FORM);
     const setName = (name) => dispatch({ type: SET_NAME_ACTION, payload: name });
     const setText = (text) => dispatch({ type: SET_TEXT_ACTION, payload: text });
