8ed00e6c40cdb0ed06671b20151aa0641d8c716c
Polina Frolova <polfroll@gmail.com>
2025-03-30 18:24:34 +0500

    hw-5

--- src/components/Review-form/use-form-reducer.js
+++ src/components/Review-form/use-form-reducer.js
@@ -6,20 +6,20 @@ const MAX = 5;
 
 export const useFormReducer = () => {
     const [form, dispatch] = useReducer(reducer, DEFAULT_VALUE_FORM);
-    const setName = useCallback((name) => dispatch({ type: SET_NAME_ACTION, payload: name }), [dispatch]);
-    const setText = useCallback((text) => dispatch({ type: SET_TEXT_ACTION, payload: text }), [dispatch]);
+    const setName = useCallback((name) => dispatch({ type: SET_NAME_ACTION, payload: name }), []);
+    const setText = useCallback((text) => dispatch({ type: SET_TEXT_ACTION, payload: text }), []);
     const { rating } = form;
     const incrementRating = useCallback(() => {
         if ( rating < MAX) {
             dispatch({ type: SET_RATING, payload: rating + 1 });
         }
-    }, [dispatch]);
+    }, []);
     const decrementRating = useCallback(() => {
         if (rating > MIN) {
             dispatch({ type: SET_RATING, payload: rating - 1 });
         }
-    }, [dispatch]);
-    const resetForm = useCallback(() => dispatch({ type: RESET_FORM }), [dispatch]);
+    }, []);
+    const resetForm = useCallback(() => dispatch({ type: RESET_FORM }), []);
 
     return {
         setName,
